b0631f6f6db270ece77356e424302d708b4ae469
Abdul Karim <abdulkarimmandvia76@gmail.com>
2024-05-05 21:25:17 +0500

    Q7 Solved Done

--- app.mjs
+++ app.mjs
@@ -17,10 +17,10 @@ for(let i = 0 ; i<number.length ; i++){
 }
 
 //Q:4
-let table = prompt("Enter Any Number :")
-for(let i = 1; i<11 ; i++){
-    console.log(`${table} x ${i} = ${table * i}`)
-}
+// let table = prompt("Enter Any Number :")
+// for(let i = 1; i<11 ; i++){
+//     console.log(`${table} x ${i} = ${table * i}`)
+// }
 
 //Q:5
 let fruits = ["apple", "banana", "mango","orange","strawberry"]
@@ -28,3 +28,26 @@ for (let i = 0 ; i<fruits.length ; i++){
     console.log(fruits[i])
     console.log(`Element at Index ${i} is ${fruits[i]}`);
 }
+
+
+//Q:6
+
+//Question is not Clear
+
+//Q:7
+let userItem = prompt("Enter Your Item:")
+let bakeryItems = ["cake", "apple pie", "cookie", "chips", "patties"]
+
+for(let i=0;i<bakeryItems.length;i++){
+    // console.log(bakeryItems[i])
+    if(userItem == bakeryItems[i]){
+        alert(`${userItem} is available at index ${i} in our bakery`)
+        break;   
+    }
+    else{
+        alert(`We are sorry.${userItem} is not available in our bakery`)
+        break;
+    }
+}
+
+//Q:8
